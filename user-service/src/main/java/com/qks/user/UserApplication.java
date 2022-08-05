1572f2854656802fc772b968a49fd8a2cf3f0d02
senluoye <57495136+senluoye@users.noreply.github.com>
2022-08-05 17:06:37 +0800

    new

---
+++ user-service/src/main/java/com/qks/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.qks.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Dessert
+ * @Description
+ * @Author QKS
+ * @Version v1.0
+ * @Create 2022-08-04 19:35
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}

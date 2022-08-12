2ac4a551973cd8b6d12f4a98462886638232fd34
senluoye <57495136+senluoye@users.noreply.github.com>
2022-08-12 20:11:16 +0800

    minio

---
+++ task-service/src/main/java/com/qks/task/confg/MinIoClientConfig.java
@@ -0,0 +1,38 @@
+package com.qks.task.confg;
+
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName Dessert
+ * @Description
+ * @Author QKS
+ * @Version v1.0
+ * @Create 2022-08-12 20:06
+ */
+@Data
+@Component
+public class MinIoClientConfig {
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    /**
+     * 注入minio 客户端
+     * @return
+     */
+    @Bean
+    public MinioClient minioClient(){
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+}
